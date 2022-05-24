dc3f5c96ae1bca9d1a61ec4b75c619e23389d260
RAHUL THAKUR <rahul.thakur@aeologic.com>
2022-05-24 16:17:04 +0530

    minor changes

--- lib/features/development/view/development_screen.dart
+++ lib/features/development/view/development_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:new_folder_structure/res/assets.gen.dart';
 import 'package:new_folder_structure/res/dimens.dart';
 
+import '../../../res/colors.gen.dart';
+
 class DevelopmentScreen extends StatelessWidget {
   const DevelopmentScreen({Key? key}) : super(key: key);
 
@@ -11,6 +13,7 @@ class DevelopmentScreen extends StatelessWidget {
       backgroundColor: Colors.amber,
       appBar: AppBar(
         title: const Text('Development Flavor'),
+        backgroundColor: ColorName.mediumSlateBlue,
       ),
       body: Center(
         child: Column(
